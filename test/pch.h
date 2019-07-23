f2d70f5f160122891019ad764fa53123d001c2c1
Matthew Glazar <strager.nds@gmail.com>
2019-07-23 16:24:39 -0700

    Precompile more headers in tests

--- test/pch.h
+++ test/pch.h
@@ -3,9 +3,17 @@
 
 #include "stringify.h"
 #include <algorithm>
+#include <array>
+#include <condition_variable>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <iterator>
+#include <random>
+#include <sys/time.h>
 #include <thread>
 #include <type_traits>
 #include <vector>
+
+#if defined(__APPLE__)
+#include <mach/mach_time.h>
+#endif
