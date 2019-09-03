b45e22147497f77c409d76f9236a720712391e23
Matthew Glazar <strager.nds@gmail.com>
2019-09-03 09:14:01 -0700

    Factor hack for waiting for thread to exit

--- test/thread.cpp
+++ test/thread.cpp
@@ -1,16 +1,33 @@
 #include "thread.h"
+#include <cassert>
 #include <cerrno>
+#include <chrono>
 #include <cstdio>
 #include <cstring>
 #include <cxxtrace/detail/have.h>
 #include <cxxtrace/string.h>
 #include <exception>
+#include <thread>
 
 #if CXXTRACE_HAVE_PTHREAD_SETNAME_NP
 #include <pthread.h>
 #endif
 
+using namespace std::literals::chrono_literals;
+
 namespace cxxtrace_test {
+auto
+await_thread_exit([[maybe_unused]] std::thread& t) -> void
+{
+  assert(!t.joinable());
+
+#if defined(__APPLE__)
+  // HACK(strager): Work around flakiness caused by std::thread::join not
+  // waiting for kernel resources to be released (with Clang 8 on macOS 10.12).
+  std::this_thread::sleep_for(10ms);
+#endif
+}
+
 auto
 set_current_thread_name(cxxtrace::czstring name) -> void
 {
--- test/thread.h
+++ test/thread.h
@@ -2,8 +2,11 @@
 #define CXXTRACE_TEST_THREAD_H
 
 #include <cxxtrace/string.h>
+#include <thread>
 
 namespace cxxtrace_test {
+auto
+await_thread_exit(std::thread&) -> void;
 auto set_current_thread_name(cxxtrace::czstring) -> void;
 }
 
