661b8507e9380aff9c94b569940c599abeb7d4c4
Matthew Glazar <strager.nds@gmail.com>
2019-09-03 19:00:13 -0700

    Refactor thread benchmarks
    Reduce code duplication by parameterizing thread benchmarks on the
    function being measured.

--- test/benchmark_thread.cpp
+++ test/benchmark_thread.cpp
@@ -5,38 +5,31 @@
 #include <cxxtrace/detail/thread.h>
 #include <cxxtrace/thread.h>
 
+using cxxtrace::detail::thread_name_set;
+
 namespace cxxtrace_test {
+namespace {
+template<cxxtrace::thread_id (*GetCurrentThreadID)() noexcept>
 auto
 benchmark_get_current_thread_id(benchmark::State& bench) -> void
 {
   for (auto _ : bench) {
-    benchmark::DoNotOptimize(cxxtrace::get_current_thread_id());
+    benchmark::DoNotOptimize(GetCurrentThreadID());
   }
 }
-BENCHMARK(benchmark_get_current_thread_id);
-
+BENCHMARK_TEMPLATE1(benchmark_get_current_thread_id,
+                    cxxtrace::get_current_thread_id);
 #if CXXTRACE_HAVE_MACH_THREAD
-auto
-benchmark_get_current_thread_mach_thread_id(benchmark::State& bench) -> void
-{
-  for (auto _ : bench) {
-    benchmark::DoNotOptimize(cxxtrace::get_current_thread_mach_thread_id());
-  }
-}
-BENCHMARK(benchmark_get_current_thread_mach_thread_id);
+BENCHMARK_TEMPLATE1(benchmark_get_current_thread_id,
+                    cxxtrace::get_current_thread_mach_thread_id);
 #endif
-
 #if CXXTRACE_HAVE_PTHREAD_THREADID_NP
-auto
-benchmark_get_current_thread_pthread_thread_id(benchmark::State& bench) -> void
-{
-  for (auto _ : bench) {
-    benchmark::DoNotOptimize(cxxtrace::get_current_thread_pthread_thread_id());
-  }
-}
-BENCHMARK(benchmark_get_current_thread_pthread_thread_id);
+BENCHMARK_TEMPLATE1(benchmark_get_current_thread_id,
+                    cxxtrace::get_current_thread_pthread_thread_id);
 #endif
 
+template<void (thread_name_set::*FetchAndRememberNameOfCurrentThread)(
+  cxxtrace::thread_id) noexcept(false)>
 auto
 benchmark_fetch_and_remember_name_of_current_thread(benchmark::State& bench)
   -> void
@@ -47,74 +40,28 @@ benchmark_fetch_and_remember_name_of_current_thread(benchmark::State& bench)
   char buffer[1024];
   for (auto _ : bench) {
     auto allocator = monotonic_buffer_resource{ buffer, sizeof(buffer) };
-    auto thread_names = cxxtrace::detail::thread_name_set{ &allocator };
-    thread_names.fetch_and_remember_name_of_current_thread(thread_id);
+    auto thread_names = thread_name_set{ &allocator };
+    (thread_names.*FetchAndRememberNameOfCurrentThread)(thread_id);
     // FIXME(strager): Is this usage of DoNotOptimize correct?
     benchmark::DoNotOptimize(thread_names);
   }
 }
-BENCHMARK(benchmark_fetch_and_remember_name_of_current_thread);
-
-#if CXXTRACE_HAVE_PROC_PIDINFO
-auto
-benchmark_fetch_and_remember_name_of_current_thread_libproc(
-  benchmark::State& bench) -> void
-{
-  set_current_thread_name("my thread name");
-
-  auto thread_id = cxxtrace::get_current_thread_id();
-  char buffer[1024];
-  for (auto _ : bench) {
-    auto allocator = monotonic_buffer_resource{ buffer, sizeof(buffer) };
-    auto thread_names = cxxtrace::detail::thread_name_set{ &allocator };
-    thread_names.fetch_and_remember_name_of_current_thread_libproc(thread_id);
-    // FIXME(strager): Is this usage of DoNotOptimize correct?
-    benchmark::DoNotOptimize(thread_names);
-  }
-}
-BENCHMARK(benchmark_fetch_and_remember_name_of_current_thread_libproc);
-#endif
-
+BENCHMARK_TEMPLATE1(
+  benchmark_fetch_and_remember_name_of_current_thread,
+  &thread_name_set::fetch_and_remember_name_of_current_thread);
 #if CXXTRACE_HAVE_MACH_THREAD
-auto
-benchmark_fetch_and_remember_name_of_current_thread_mach(
-  benchmark::State& bench) -> void
-{
-  set_current_thread_name("my thread name");
-
-  auto thread_id = cxxtrace::get_current_thread_id();
-  char buffer[1024];
-  for (auto _ : bench) {
-    auto allocator = monotonic_buffer_resource{ buffer, sizeof(buffer) };
-    auto thread_names = cxxtrace::detail::thread_name_set{ &allocator };
-    thread_names.fetch_and_remember_name_of_current_thread_mach(thread_id);
-    // FIXME(strager): Is this usage of DoNotOptimize correct?
-    benchmark::DoNotOptimize(thread_names);
-  }
-}
-BENCHMARK(benchmark_fetch_and_remember_name_of_current_thread_mach);
+BENCHMARK_TEMPLATE1(
+  benchmark_fetch_and_remember_name_of_current_thread,
+  &thread_name_set::fetch_and_remember_name_of_current_thread_mach);
 #endif
-
 #if CXXTRACE_HAVE_PTHREAD_GETNAME_NP
-auto
-benchmark_fetch_and_remember_name_of_current_thread_pthread(
-  benchmark::State& bench) -> void
-{
-  set_current_thread_name("my thread name");
-
-  auto thread_id = cxxtrace::get_current_thread_id();
-  char buffer[1024];
-  for (auto _ : bench) {
-    auto allocator = monotonic_buffer_resource{ buffer, sizeof(buffer) };
-    auto thread_names = cxxtrace::detail::thread_name_set{ &allocator };
-    thread_names.fetch_and_remember_name_of_current_thread_pthread(thread_id);
-    // FIXME(strager): Is this usage of DoNotOptimize correct?
-    benchmark::DoNotOptimize(thread_names);
-  }
-}
-BENCHMARK(benchmark_fetch_and_remember_name_of_current_thread_pthread);
+BENCHMARK_TEMPLATE1(
+  benchmark_fetch_and_remember_name_of_current_thread,
+  &thread_name_set::fetch_and_remember_name_of_current_thread_pthread);
 #endif
 
+template<void (thread_name_set::*FetchAndRememberThreadNameForID)(
+  cxxtrace::thread_id) noexcept(false)>
 auto
 benchmark_fetch_and_remember_name_of_current_thread_by_thread_id(
   benchmark::State& bench) -> void
@@ -125,32 +72,19 @@ benchmark_fetch_and_remember_name_of_current_thread_by_thread_id(
   char buffer[1024];
   for (auto _ : bench) {
     auto allocator = monotonic_buffer_resource{ buffer, sizeof(buffer) };
-    auto thread_names = cxxtrace::detail::thread_name_set{ &allocator };
-    thread_names.fetch_and_remember_thread_name_for_id(thread_id);
+    auto thread_names = thread_name_set{ &allocator };
+    (thread_names.*FetchAndRememberThreadNameForID)(thread_id);
     // FIXME(strager): Is this usage of DoNotOptimize correct?
     benchmark::DoNotOptimize(thread_names);
   }
 }
-BENCHMARK(benchmark_fetch_and_remember_name_of_current_thread_by_thread_id);
-
+BENCHMARK_TEMPLATE(
+  benchmark_fetch_and_remember_name_of_current_thread_by_thread_id,
+  &thread_name_set::fetch_and_remember_thread_name_for_id);
 #if CXXTRACE_HAVE_PROC_PIDINFO
-auto
-benchmark_fetch_and_remember_name_of_current_thread_by_thread_id_libproc(
-  benchmark::State& bench) -> void
-{
-  set_current_thread_name("my thread name");
-
-  auto thread_id = cxxtrace::get_current_thread_id();
-  char buffer[1024];
-  for (auto _ : bench) {
-    auto allocator = monotonic_buffer_resource{ buffer, sizeof(buffer) };
-    auto thread_names = cxxtrace::detail::thread_name_set{ &allocator };
-    thread_names.fetch_and_remember_thread_name_for_id_libproc(thread_id);
-    // FIXME(strager): Is this usage of DoNotOptimize correct?
-    benchmark::DoNotOptimize(thread_names);
-  }
-}
-BENCHMARK(
-  benchmark_fetch_and_remember_name_of_current_thread_by_thread_id_libproc);
+BENCHMARK_TEMPLATE(
+  benchmark_fetch_and_remember_name_of_current_thread_by_thread_id,
+  &thread_name_set::fetch_and_remember_thread_name_for_id_libproc);
 #endif
+}
 }
