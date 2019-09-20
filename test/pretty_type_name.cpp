7dd2f6ec775d935b941b03b1c71c1185c2310de2
Matthew Glazar <strager.nds@gmail.com>
2019-09-20 11:45:32 -0700

    Strip namespaces from concurrency test names
    Before:
    
        Running cxxtrace_test::ring_queue_push_one_pop_all_relacy_test<cxxtrace::detail::spmc_ring_queue<int, 64ul, int> >...
        Passed after 13932 test executions
        Running cxxtrace_test::ring_queue_overflow_drops_some_but_not_all_items_relacy_test<cxxtrace::detail::spmc_ring_queue<int, 4ul, int> >...
        Passed after 13428 test executions
    
    After:
    
        Running ring_queue_push_one_pop_all_relacy_test<spmc_ring_queue<int, 64ul, int> >...
        Passed after 14169 test executions
        Running ring_queue_overflow_drops_some_but_not_all_items_relacy_test<spmc_ring_queue<int, 4ul, int> >...
        Passed after 12996 test executions

--- test/pretty_type_name.cpp
+++ test/pretty_type_name.cpp
@@ -1,7 +1,9 @@
 #include "pretty_type_name.h"
+#include <cstddef>
 #include <cxxtrace/detail/have.h>
 #include <cxxtrace/string.h>
 #include <string>
+#include <string_view>
 #include <typeinfo>
 
 #if CXXTRACE_HAVE_CXA_DEMANGLE
@@ -17,6 +19,23 @@ struct free_deleter
   auto operator()(void* p) const noexcept -> void { std::free(p); }
 };
 #endif
+
+auto
+strip_all(std::string_view string_to_strip, std::string& out)
+{
+  auto i = std::size_t{ 0 };
+  for (;;) {
+    if (i >= out.size()) {
+      return;
+    }
+    auto strip_index = out.find(string_to_strip, i);
+    if (strip_index == out.npos) {
+      return;
+    }
+    out.erase(strip_index, string_to_strip.size());
+    i = strip_index;
+  }
+}
 }
 
 namespace cxxtrace_test {
@@ -24,16 +43,27 @@ auto
 pretty_type_name(const std::type_info& type) -> std::string
 {
   const auto* type_name = cxxtrace::czstring{ type.name() };
+  auto result = std::string{};
 
 #if CXXTRACE_HAVE_CXA_DEMANGLE
   auto status = int{};
   auto buffer = std::unique_ptr<char, free_deleter>{ abi::__cxa_demangle(
     type_name, nullptr, nullptr, &status) };
   if (buffer && status == 0) {
-    return std::string{ buffer.get() };
+    result = std::string{ buffer.get() };
+  } else {
+    result = type_name;
   }
+#else
+  result = type_name;
 #endif
 
-  return type_name;
+  strip_all("cxxtrace::detail::", result);
+  strip_all("cxxtrace_test::detail::", result);
+
+  strip_all("cxxtrace::", result);
+  strip_all("cxxtrace_test::", result);
+
+  return result;
 }
 }
