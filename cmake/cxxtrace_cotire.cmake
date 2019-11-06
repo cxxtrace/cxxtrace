7411ca79ee669309cb42dcd6852814f5ec4de9ce
Matthew Glazar <strager.nds@gmail.com>
2019-11-05 16:33:23 -0800

    Fix PCH for test_processor_id_manual on Linux
    test_processor_id_manual depends on cxxtrace, and cxxtrace depends on
    Threads::Threads. On Linux, this means the compiler sees -D_REENTRANT.
    
    test_processor_id_manual does not depend on Threads::Threads itself.
    On Linux, this means cotire does *not* see -D_REENTRANT, so cotire
    pre-compiles headers without -D_REENTRANT.
    
    The difference in compiler flags between the pre-compiled header and the
    build causes problems. See inline comment for details.
    
    Hack around the problem by having test_processor_id_manual depend on
    Threads::Threads directly.

--- cmake/cxxtrace_cotire.cmake
+++ cmake/cxxtrace_cotire.cmake
@@ -2,7 +2,25 @@ cmake_minimum_required(VERSION 3.10)
 
 include(cotire)
 
-function (CXXTRACE_COTIRE)
+function (CXXTRACE_COTIRE TARGET)
+  # HACK(strager): On Linux, in the following scenario, pre-compiled header
+  # generation is buggy:
+  #
+  # * Target A depends on target B.
+  # * Target B depends on Threads::Threads.
+  # * cxxtrace_cotire is called on target A.
+  #
+  # In this case, cotire pre-compiles headers for target A without -D_REENTRANT,
+  # but the build system compiles target A's sources with -D_REENTRANT. GCC
+  # warns about this discrepancy, and Clang reports a fatal error.
+  #
+  # As a workaround, make all targets explicitly depend on Threads::Threads
+  # (thus both cotire and the build system see -D_REENTRANT).
+  find_package(Threads)
+  if (TARGET Threads::Threads)
+    target_link_libraries(${TARGET} PRIVATE Threads::Threads)
+  endif ()
+
   # HACK(strager): If CMAKE_INCLUDE_SYSTEM_FLAG_CXX is "-isystem ", cotire
   # invokes the compiler with "-isystem /path/to/include" as one argument. This
   # confuses Clang. Make cotire invoke the compiler with "-isystem" and
