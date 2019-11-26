f7dff43f204f23c9e8ca39b08ed5f43f6bafaff4
Matthew Glazar <strager.nds@gmail.com>
2019-11-25 21:59:28 -0800

    f rseq

--- test/concurrency_stress_synchronization.h
+++ test/concurrency_stress_synchronization.h
@@ -14,12 +14,28 @@
 
 namespace cxxtrace_test {
 class concurrency_stress_synchronization
-  : public cxxtrace::detail::real_synchronization
+  : private cxxtrace::detail::real_synchronization
   , public rseq_scheduler_synchronization_mixin<
       concurrency_stress_synchronization,
       cxxtrace::detail::real_synchronization::debug_source_location>
 {
+private:
+  using real_synchronization = cxxtrace::detail::real_synchronization;
+  using rseq_scheduler_synchronization_mixin = rseq_scheduler_synchronization_mixin<
+      concurrency_stress_synchronization,
+      real_synchronization::debug_source_location>;
+
 public:
+  using real_synchronization::atomic;
+  using real_synchronization::atomic_flag;
+  using real_synchronization::atomic_thread_fence;
+  using real_synchronization::backoff;
+  using real_synchronization::debug_source_location;
+  using real_synchronization::nonatomic;
+  using rseq_scheduler_synchronization_mixin::allow_preempt;
+  using rseq_scheduler_synchronization_mixin::get_rseq;
+  using rseq_scheduler_synchronization_mixin::rseq_handle;
+
   using relaxed_semaphore =
 #if CXXTRACE_HAVE_LIBDISPATCH_SEMAPHORE
     libdispatch_semaphore
@@ -32,10 +48,6 @@ public:
 
   template<class T>
   using thread_local_var = pthread_thread_local_var<T>;
-
-  using rseq_scheduler_synchronization_mixin<
-    concurrency_stress_synchronization,
-    debug_source_location>::allow_preempt;
 };
 
 extern template class rseq_scheduler_synchronization_mixin<
